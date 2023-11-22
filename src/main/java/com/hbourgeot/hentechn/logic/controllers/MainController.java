4d12786845c62f0601887debffd3e10d1d97bf96
Henrry Bourgeot <henrrybrgt@gmail.com>
2023-11-22 08:18:10 -0400

    adding swagger-ui

--- src/main/java/com/hbourgeot/hentechn/logic/controllers/MainController.java
+++ src/main/java/com/hbourgeot/hentechn/logic/controllers/MainController.java
@@ -1,4 +1,4 @@
-package com.hbourgeot.hentechn.controllers;
+package com.hbourgeot.hentechn.logic.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
