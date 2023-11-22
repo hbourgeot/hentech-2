977fcd73c1d0c4c18feb004abdf4948433464f43
Henrry Bourgeot <henrrybrgt@gmail.com>
2023-11-21 22:24:02 -0400

    first commit

---
+++ src/main/java/com/hbourgeot/hentechn/HentechnApplication.java
@@ -0,0 +1,13 @@
+package com.hbourgeot.hentechn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HentechnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HentechnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hbourgeot/hentechn/HentechnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbourgeot.hentechn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HentechnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
